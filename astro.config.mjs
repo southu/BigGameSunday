a64a8b8e4b0f09f230b551b0dabe9078e76c8954
southu <jsnhrpr@gmail.com>
2026-01-30 12:25:35 -0500

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://biggamesunday.com',
   trailingSlash: 'never',
   output: 'server',
-  adapter: netlify(),
+  adapter: node({ mode: 'standalone' }),
   build: {
     format: 'file'
   },
